00a519e6ba288475086236e457bfece7dc27d1e1
Danny Shen <shenfive@hotmail.com>
2018-07-25 15:23:36 +0800

    Initial Commit

---
+++ Play It/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Play It
+//
+//  Created by 申潤五 on 2018/7/25.
+//  Copyright © 2018年 申潤五. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
